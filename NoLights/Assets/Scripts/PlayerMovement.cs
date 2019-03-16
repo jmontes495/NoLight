d593664363d0f21733a9c74705b14bc9b880ef29
Juliana Montes <jmontes@jamcity.com>
2019-03-16 12:25:34 -0500

    Created 2D movement

---
+++ NoLights/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float tileDistance;
+    [SerializeField]
+    private float movementDelay;
+
+    private Transform myTransform;    
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        myTransform = transform;
+        StartCoroutine(Walking());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Cursor.lockState = CursorLockMode.None;
+    }
+
+    private IEnumerator Walking()
+    {
+        WaitForSeconds delay = new WaitForSeconds(movementDelay);        
+
+        while (true)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = Vector3.zero;
+
+            if (moveHorizontal < 0)
+                movement.x = -tileDistance;
+            else if (moveHorizontal > 0)
+                movement.x = tileDistance;
+            else if (moveVertical > 0)
+                movement.y = tileDistance;
+            else if (moveVertical < 0)
+                movement.y = -tileDistance;
+
+            myTransform.position += movement;
+
+            yield return delay;
+        }
+    }
+}
